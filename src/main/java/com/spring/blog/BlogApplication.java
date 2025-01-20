6abda27938d356f9ae269a2ad63d654a3aa4d1e7
YUNHWAN <629jyh7@naver.com>
2025-01-20 22:46:28 +0900

    feat: yml 설정 적용 및 profile 통합

--- src/main/java/com/spring/blog/BlogApplication.java
+++ src/main/java/com/spring/blog/BlogApplication.java
@@ -9,13 +9,20 @@ public class BlogApplication {
 
 	public static void main(String[] args) {
 
-//		// .env 파일 로드
-//		Dotenv dotenv = Dotenv.configure().load();
-//
-//		// 환경 변수 값으로 시스템 속성 설정
-//		System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
-//		System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
-//		System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
+		// .env 파일 로드
+		Dotenv dotenv = Dotenv.load();
+
+		// 환경 변수 값으로 시스템 속성 설정
+		System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
+		System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
+		System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
+
+		System.setProperty("google.client-id", dotenv.get("GOOGLE_CLIENT_ID"));
+		System.setProperty("google.client-secret", dotenv.get("GOOGLE_CLIENT_SECRET"));
+
+		System.setProperty("jwt.secret", dotenv.get("JWT_SECRET"));
+
+		System.setProperty("spring.profiles.active", dotenv.get("SPRING_PROFILES_ACTIVE"));
 
 		SpringApplication.run(BlogApplication.class, args);
 	}
